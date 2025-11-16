02f530e10b47beb3282000dbdc7564fbb3829730
Ramón Panadero <55316473+lionlightheart@users.noreply.github.com>
2025-11-16 19:28:07 +0100

    feat: add MailModule to handle email functionality

--- nayaview-anime-back/src/app.module.ts
+++ nayaview-anime-back/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
+import { MailModule } from './mail/mail.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), UsersModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true, envFilePath: '.env' }), UsersModule, MailModule],
   controllers: [AppController],
   providers: [AppService],
 })
